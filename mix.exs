9b5662cb82f0ca694fcaf76d95adce8ae83f3188
Kyle A Anderson <kyleaa@gmail.com>
2019-01-07 08:11:08 -0800

    update ex_doc version

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule ExAws.RDS.Mixfile do
       {:hackney, ">= 0.0.0", only: [:dev, :test]},
       {:sweet_xml, ">= 0.0.0", only: [:dev, :test]},
       {:poison, ">= 0.0.0", only: [:dev, :test]},
-      {:ex_doc, "~> 0.16", only: [:dev, :test]},
+      {:ex_doc, "~> 0.19", only: [:dev, :test]},
       ex_aws(),
     ]
   end
