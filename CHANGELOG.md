eb1e21dcf9e65c6fef7852e5eeb634879ee8ebbf
fmcgeough <fmcgeough@gmail.com>
2019-04-10 11:12:24 -0400

    Enhancement - new API function remove_tags_from_resource

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,12 @@
+v2.0.3
+
+- Enhancement: Add function remove_tags_from_resource
+- Maintenance: Add doc and spec for add_tags_to_resource
+
 v2.0.2
 
 - Enhancement: Add functions describe_pending_maintenance_actions, describe_db_clusters, list_tags_for_resource
-- Maintenance: Reformat code with Elixir formatter 
+- Maintenance: Reformat code with Elixir formatter
 
 v2.0.1
 
--- mix.exs
+++ mix.exs
@@ -4,19 +4,19 @@ defmodule ExAws.RDS.Mixfile do
   def project do
     [
       app: :ex_aws_rds,
-      version: "2.0.2",
+      version: "2.0.3",
       elixir: "~> 1.5",
-      elixirc_paths: elixirc_paths(Mix.env),
+      elixirc_paths: elixirc_paths(Mix.env()),
       description: "ExAws.RDS service package",
       source_url: "https://github.com/ex-aws/ex_aws_rds",
       package: package(),
-      start_permanent: Mix.env == :prod,
+      start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib",]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help compile.app" to learn about applications.
   def application do
@@ -32,7 +32,7 @@ defmodule ExAws.RDS.Mixfile do
       {:sweet_xml, ">= 0.0.0", only: [:dev, :test]},
       {:poison, ">= 0.0.0", only: [:dev, :test]},
       {:ex_doc, "~> 0.19", only: [:dev, :test]},
-      ex_aws(),
+      ex_aws()
     ]
   end
 
